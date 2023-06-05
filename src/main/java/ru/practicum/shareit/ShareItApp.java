019bc93c9c5ff1e54615782dc9c6f5e7925841af
Dima <cinnamonbun1233@yandex.ru>
2023-06-05 21:28:20 +0300

    Update

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShareItApp {
     public static void main(String[] args) {
         SpringApplication.run(ShareItApp.class, args);
+
     }
 }
