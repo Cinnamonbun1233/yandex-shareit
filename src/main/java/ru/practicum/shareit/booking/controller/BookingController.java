c947a67bb4737880e2dbf7146908ea2f01fafb92
Dima <cinnamonbun1233@yandex.ru>
2023-06-08 16:02:21 +0300

    Update

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -2,10 +2,10 @@ package ru.practicum.shareit.booking.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.booking.status.State;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
 import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.booking.status.State;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -15,34 +15,36 @@ import java.util.List;
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor
 public class BookingController {
-    private final BookingService bookingServiceImpl;
+    private final BookingService bookingService;
 
     @PostMapping
-    public BookingResponseDto addBooking(@RequestBody @Valid BookingRequestDto dto, @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
-        return bookingServiceImpl.createNewBooking(dto, userId);
+    public BookingResponseDto createNewBooking(@RequestBody @Valid BookingRequestDto bookingRequestDto,
+                                               @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
+        return bookingService.createNewBooking(bookingRequestDto, userId);
+    }
+
+    @GetMapping
+    public List<BookingResponseDto> getAllUserBookings(@RequestParam(defaultValue = "ALL") State state,
+                                                       @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
+        return bookingService.getAllUserBookings(state, userId, false);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingResponseDto> getAllUserItemBookings(@RequestParam(defaultValue = "ALL") State state,
+                                                           @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
+        return bookingService.getAllUserBookings(state, userId, true);
     }
 
     @GetMapping("/{bookingId}")
-    public BookingResponseDto getBookingById(@PathVariable Long bookingId, @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
-        return bookingServiceImpl.getBookingById(bookingId, userId);
+    public BookingResponseDto getBookingById(@PathVariable Long bookingId,
+                                             @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
+        return bookingService.getBookingById(bookingId, userId);
     }
 
     @PatchMapping("/{bookingId}")
     public BookingResponseDto approveBooking(@PathVariable Long bookingId,
                                              @RequestParam Boolean approved,
                                              @RequestHeader("X-Sharer-User-Id") @NotNull Long ownerId) {
-        return bookingServiceImpl.approveBooking(bookingId, approved, ownerId);
-    }
-
-    @GetMapping
-    public List<BookingResponseDto> getAllUserBookings(@RequestParam(defaultValue = "ALL") State state,
-                                                       @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
-        return bookingServiceImpl.getAllUserBookings(state, userId, false);
-    }
-
-    @GetMapping("/owner")
-    public List<BookingResponseDto> getAllUserItemBookings(@RequestParam(defaultValue = "ALL") State state,
-                                                           @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
-        return bookingServiceImpl.getAllUserBookings(state, userId, true);
+        return bookingService.approveBooking(bookingId, approved, ownerId);
     }
 }
--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -8,22 +8,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-import ru.practicum.shareit.booking.status.BookingStatus;
-import ru.practicum.shareit.booking.status.State;
-import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
-import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.mapper.BookingMapper;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.repository.BookingRepository;
+import ru.practicum.shareit.booking.status.BookingStatus;
+import ru.practicum.shareit.booking.status.State;
 import ru.practicum.shareit.exception.*;
-import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.repository.UserRepository;
+import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static ru.practicum.shareit.booking.model.QBooking.booking;
 
@@ -36,71 +37,95 @@ public class BookingServiceImpl implements BookingService {
     private final UserRepository userRepository;
 
     @Transactional
-    public BookingResponseDto createNewBooking(BookingRequestDto dto, Long userId) {
-        Item item = itemRepository.findById(dto.getItemId()).orElseThrow(() ->
-                new ItemNotFoundException(String.format("Вещь с id: %s не обнаружена", dto.getItemId())));
+    public BookingResponseDto createNewBooking(BookingRequestDto bookingRequestDto, Long userId) {
+        Item item = getItemRepo(bookingRequestDto);
 
         if (!item.getAvailable()) {
-            throw new ItemNotAvailableException(String.format("Вещь с id: %s недоступна для брони", item.getId()));
+            throw new ItemNotAvailableException("Предмет с id: '" + item.getId() + "' недоступен для брони");
         }
+
         User user = userRepository.findById(userId).orElseThrow(() ->
-                new UserNotFoundException(String.format("Пользователь с id: %s не обнаружен", userId)));
+                new UserNotFoundException("Пользователь с id: '" + userId + "' не найден"));
+
         if (item.getOwner().equals(user)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Бронь для владельца вещи недоступна");
         }
-        Booking booking = BookingMapper.bookingRequestDtoToBooking(dto, item, user);
 
+        Booking booking = BookingMapper.bookingRequestDtoToBooking(bookingRequestDto, item, user);
         return BookingMapper.bookingToBookingResponseDto(bookingRepository.save(booking));
     }
 
+    public List<BookingResponseDto> getAllUserBookings(State state, Long userId, boolean owner) {
+        List<Predicate> predicates = new ArrayList<>();
+        LocalDateTime now = LocalDateTime.now();
+
+        if (owner) {
+            predicates.add(booking.item.owner.id.eq(userId));
+        } else {
+            predicates.add(booking.booker.id.eq(userId));
+        }
+
+        stateSwitcher(state, predicates, now);
+        List<BookingResponseDto> bookingResponseDtoList = BookingMapper.bookingToBookingResponseDto(
+                bookingRepository.findAll(Objects.requireNonNull(ExpressionUtils.allOf(predicates)),
+                        Sort.by(Sort.Direction.DESC, "startDate")));
+
+        if (bookingResponseDtoList.isEmpty())
+            throw new BookingNotFoundException("Пользователь с id: '" + userId + "' не имеет брони");
+
+        return bookingResponseDtoList;
+    }
+
     public BookingResponseDto getBookingById(Long bookingId, Long userId) {
-        Booking booking = bookingRepository.findBooking(bookingId, userId)
-                .orElseThrow(() -> new BookingNotFoundException(String.format("Бронь с id: %s не обнаружена", bookingId)));
+        Booking booking = bookingRepository.findBooking(bookingId, userId).orElseThrow(()
+                -> new BookingNotFoundException("Бронь с id: '" + bookingId + "' не обнаружена"));
         return BookingMapper.bookingToBookingResponseDto(booking);
     }
 
     @Transactional
     public BookingResponseDto approveBooking(Long bookingId, Boolean approved, Long ownerId) {
-        Booking booking = bookingRepository.findBookingByOwner(bookingId, ownerId)
-                .orElseThrow(() -> new BookingNotFoundException(
-                        String.format("Бронь с id: %s для владельца с id: %s не обнаружена", bookingId, ownerId)));
+        Booking booking = bookingRepository.findBookingByOwner(bookingId, ownerId).orElseThrow(()
+                -> new BookingNotFoundException("Бронь с id: '" + bookingId + "' для пользователя с id: '"
+                + ownerId + "' не обнаружена"));
         checkAlreadyApproved(booking);
+
         if (approved) {
             booking.setStatus(BookingStatus.APPROVED);
         } else {
             booking.setStatus(BookingStatus.REJECTED);
         }
+
         return BookingMapper.bookingToBookingResponseDto(bookingRepository.save(booking));
     }
 
     private void checkAlreadyApproved(Booking booking) {
         BookingStatus status = booking.getStatus();
+
         if (booking.getStatus() != null && (status == BookingStatus.APPROVED || status == BookingStatus.REJECTED)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     "Невозможно изменить статус аренды после подтверждения");
         }
     }
 
-    public List<BookingResponseDto> getAllUserBookings(State state, Long userId, boolean owner) {
-        List<Predicate> predicates = new ArrayList<>();
-        LocalDateTime curTime = LocalDateTime.now();
-        if (owner) {
-            predicates.add(booking.item.owner.id.eq(userId));
-        } else {
-            predicates.add(booking.booker.id.eq(userId));
-        }
+
+    private Item getItemRepo(BookingRequestDto bookingRequestDto) {
+        return itemRepository.findById(bookingRequestDto.getItemId()).orElseThrow(() ->
+                new ItemNotFoundException("Предмет с id: '" + bookingRequestDto.getItemId() + "' не найден"));
+    }
+
+    private static void stateSwitcher(State state, List<Predicate> predicates, LocalDateTime now) {
         switch (state) {
             case ALL:
                 break;
             case FUTURE:
-                predicates.add(booking.startDate.after(curTime));
+                predicates.add(booking.startDate.after(now));
                 break;
             case PAST:
-                predicates.add(booking.endDate.before(curTime));
+                predicates.add(booking.endDate.before(now));
                 break;
             case CURRENT:
-                predicates.add(booking.startDate.loe(curTime)
-                        .and(booking.endDate.gt(curTime)));
+                predicates.add(booking.startDate.loe(now)
+                        .and(booking.endDate.gt(now)));
                 break;
             case REJECTED:
                 predicates.add(booking.status.eq(BookingStatus.REJECTED));
@@ -111,13 +136,5 @@ public class BookingServiceImpl implements BookingService {
             default:
                 throw new UnknownStateException(State.UNSUPPORTED_STATUS.name());
         }
-        List<BookingResponseDto> dtos = BookingMapper.bookingToBookingResponseDto(
-                bookingRepository.findAll(ExpressionUtils.allOf(predicates),
-                        Sort.by(Sort.Direction.DESC, "startDate")));
-
-        if (dtos.isEmpty())
-            throw new BookingNotFoundException(String.format("Пользователь с id : %s не имеет брони", userId));
-
-        return dtos;
     }
 }
